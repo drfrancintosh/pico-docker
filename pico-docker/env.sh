12690b7f5cf93308161566d8ec265a355d7f5483
Gregory Smith <greg@agilefrontiers.com>
2024-11-12 01:17:39 -0500

    removing copy

--- pico-docker/env.sh
+++
@@ -1,6 +0,0 @@
-# environment vars
-export DOCKER="podman"
-export PICO_IMAGE="pico-sdk-2-image"
-export PICO_CONTAINER="pico-sdk-2"
-export DEFAULT_MACHINE="podman-machine-default"
-
--- pico-docker/scripts/login.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /home
-source .bashrc
-echo ""
-echo "Welcome to PICO SDK 2.0"
-echo ""
